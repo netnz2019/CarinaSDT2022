186284aa16f93b941f7d6567cdecb09c3f9c3f6e
spenglerc <104952542+spenglerc@users.noreply.github.com>
2022-07-27 09:53:56 +1200

    Update and rename Books.java to Book.java

---
+++ LibraryManagement/Book.java
@@ -0,0 +1,82 @@
+package book;
+
+/**
+ * @ Author 12629
+ * @ Date 2022/4/10 11:41
+ * @ Description：
+ */
+public class Book {
+    private String name;// title
+    private String author;// author
+    private int price;// Price
+    private String type;// type
+    private int id;// Book number
+    private boolean isBorrowed;// Is it lent
+
+    public Book(int id,String name, String author, int price, String type) {
+        this.name = name;
+        this.author = author;
+        this.price = price;
+        this.type = type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public boolean isBorrowed() {
+        return isBorrowed;
+    }
+
+    public void setBorrowed(boolean borrowed) {
+        isBorrowed = borrowed;
+    }
+
+        public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+    
+    @Override
+    public String toString() {
+        return "Book{" +
+                "name='" + name + '\'' +
+                ", author='" + author + '\'' +
+                ", price=" + price +
+                ", type='" + type + '\'' +
+                ", id=" + id +
+                ", isBorrowed=" + isBorrowed +
+                '}';
+    }
+}
