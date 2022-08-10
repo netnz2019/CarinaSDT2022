47badfcf264bff5f65ac2c86c2bb32c9bebc3eaa
spenglerc <104952542+spenglerc@users.noreply.github.com>
2022-08-11 09:43:28 +1200

    Add files via upload

---
+++ book.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package library;
+
+import java.util.Scanner;
+
+
+
+public class book {
+
+public int sNo;
+public String bookName;
+public String authorName;
+public int bookQty;
+public int bookQtyCopy;
+
+Scanner input = new Scanner(System.in);
+
+public book(){
+
+    System.out.println("Enter Serial No of Book:");
+    this.sNo = input.nextInt();
+    input.nextLine();
+    System.out.println("Enter Book Name:");
+    this.bookName = input.nextLine();
+    System.out.println("Enter Author Name:");
+    this.authorName = input.nextLine();
+    System.out.println("Enter Quantity of Books:");
+    this.bookQty = input.nextInt();
+    bookQtyCopy = this.bookQty;
+
+}
+
+}/**/
