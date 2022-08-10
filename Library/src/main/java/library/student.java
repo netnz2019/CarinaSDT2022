929c517fd9d9f606d267f3c6a6c428e4ec1c20a3
spenglerc <104952542+spenglerc@users.noreply.github.com>
2022-08-11 11:39:49 +1200

    move files

---
+++ Library/src/main/java/library/student.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package library;
+
+import java.util.Scanner;    
+
+/**
+ *
+ * @author gamer
+ */
+public class student {
+
+String studentName;
+String regNum;
+
+book borrowedBooks[] = new book[3];
+public int booksCount = 0;
+
+Scanner input = new Scanner(System.in);
+
+public student(){
+
+    System.out.println("Enter Student Name:");
+    this.studentName = input.nextLine();
+
+    System.out.println("Enter Reg Number:");
+    this.regNum = input.nextLine();
+
+}
+}
